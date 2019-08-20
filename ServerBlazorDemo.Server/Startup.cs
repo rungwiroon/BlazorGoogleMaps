6d763e423515c2f11afa8f6542901112853c0ba1
valentasm <gudrutis16@gmail.com>
2019-08-20 11:43:01 +0300

    Revert "Updated to work with preview 8"
    This reverts commit debc09fec974f5729d852c5f4dff2b4ff2bf90cc.

--- ServerBlazorDemo.Server/Startup.cs
+++ ServerBlazorDemo.Server/Startup.cs
@@ -33,17 +33,14 @@ namespace ServerBlazorDemo.Server
                 app.UseBlazorDebugging();
             }
 
-            app.UseStaticFiles();
-            app.UseClientSideBlazorFiles<Client.Startup>();
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
-                endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
             });
 
+            app.UseBlazor<Client.Startup>();
         }
     }
 }
