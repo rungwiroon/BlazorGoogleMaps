9dc5515cacc2820902d7affac98fd61ed69d14de
Rez <rezghelich@gmail.com>
2020-06-02 13:22:00 +1200

    Implemented PlacePlusCode type as well as adding it as setting property name for plus code in PlaceResult class

--- GoogleMapsComponents/Maps/Places/PlacePlusCode.cs
+++ GoogleMapsComponents/Maps/Places/PlacePlusCode.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace GoogleMapsComponents.Maps.Places
 {
     public class PlacePlusCode
     {
+        /// <summary>
+        /// It is  a 6 character or longer local code with an explicit location (CWC8+R9, Mountain View, CA, USA)
+        /// </summary>
+        [JsonPropertyName("compound_code")]
+        public string CompoundCode { get; set; }
+
+        /// <summary>
+        /// It is a 4 character area code and 6 character or longer local code (849VCWC8+R9)
+        /// </summary>
+        [JsonPropertyName("global_code")]
+        public string GlobalCode { get; set; }
     }
 }
