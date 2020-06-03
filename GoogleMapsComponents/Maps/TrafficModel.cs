0563db45dca503be7b0c2c04a807aa16eae0110e
Nick Barrett <nick@opscentre.co.nz>
2020-06-04 09:12:37 +1200

    TrafficModel enum needs to be serialised as lowercase text rather than ordinal.

--- GoogleMapsComponents/Maps/TrafficModel.cs
+++ GoogleMapsComponents/Maps/TrafficModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,21 +11,23 @@ namespace GoogleMapsComponents.Maps
     /// The assumptions to use when predicting duration in traffic. 
     /// Specified as part of a DirectionsRequest or DistanceMatrixRequest. 
     /// </summary>
+
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum TrafficModel
     {
         /// <summary>
         /// Use historical traffic data to best estimate the time spent in traffic.
         /// </summary>
-        BestGuess,
+        bestguess,
 
         /// <summary>
         /// Use historical traffic data to make an optimistic estimate of what the duration in traffic will be.
         /// </summary>
-        Optimistic,
+        optimistic,
 
         /// <summary>
         /// Use historical traffic data to make a pessimistic estimate of what the duration in traffic will be.
         /// </summary>
-        Pessimistic
+        pessimistic
     }
 }
