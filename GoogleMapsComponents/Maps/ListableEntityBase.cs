58058c800ff0c181872ff72752f9d9804f54e7fa
Michael Ploy (MPl) <ploy@fotec.at>
2021-10-11 16:47:20 +0200

    InvokeAsync for Polyline&Polygon

--- GoogleMapsComponents/Maps/ListableEntityBase.cs
+++ GoogleMapsComponents/Maps/ListableEntityBase.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using OneOf;
 
 namespace GoogleMapsComponents.Maps
 {
@@ -14,7 +15,7 @@ namespace GoogleMapsComponents.Maps
         public readonly Dictionary<string, List<MapEventListener>> EventListeners;
 
         public Guid Guid => _jsObjectRef.Guid;
-
+        
         internal ListableEntityBase(JsObjectRef jsObjectRef)
         {
             _jsObjectRef = jsObjectRef;
@@ -97,6 +98,26 @@ namespace GoogleMapsComponents.Maps
                 //so, instead to clear the list and remove the key from dictionary, I prefer to leave the key with an empty list
                 EventListeners[eventName].Clear();
             }
+        }
+
+        public Task InvokeAsync(string functionName, params object[] args)
+        {
+            return _jsObjectRef.InvokeAsync(functionName, args);
+        }
+
+        public Task<T> InvokeAsync<T>(string functionName, params object[] args)
+        {
+            return _jsObjectRef.InvokeAsync<T>(functionName, args);
+        }
+
+        public Task<OneOf<T, U>> InvokeAsync<T, U>(string functionName, params object[] args)
+        {
+            return _jsObjectRef.InvokeAsync<T, U>(functionName, args);
+        }
+
+        public Task<OneOf<T, U, V>> InvokeAsync<T, U, V>(string functionName, params object[] args)
+        {
+            return _jsObjectRef.InvokeAsync<T, U, V>(functionName, args);
         }
     }
 }
