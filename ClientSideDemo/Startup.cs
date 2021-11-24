a402872a8f439a020210dbc4607873f9c43ed804
Rungwiroon Komalitipong <rungwiroon@gmail.com>
2021-11-24 21:36:18 +0700

    Rename client side demo project

--- ClientSideDemo/Startup.cs
+++ ClientSideDemo/Startup.cs
@@ -1,17 +1,17 @@
-using Microsoft.AspNetCore.Components.Builder;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace ClientSideDemo
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+    public class Startup {
 
-        public void Configure(IComponentsApplicationBuilder app)
-        {
-            app.AddComponent<App>("app");
+        private static async Task Main(string[] args) {
+
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
         }
     }
 }
