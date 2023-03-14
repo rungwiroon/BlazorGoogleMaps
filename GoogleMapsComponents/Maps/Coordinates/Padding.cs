ca5cdea002bb93ae3d0019c3ce62cab45308fa8b
Vishal Jani <me@vishaljani.in>
2023-03-14 13:51:09 +0530

    added individual padding for top, left, right for fit-bounds

--- GoogleMapsComponents/Maps/Coordinates/Padding.cs
+++ GoogleMapsComponents/Maps/Coordinates/Padding.cs
@@ -1,6 +1,38 @@
-﻿namespace GoogleMapsComponents.Maps.Coordinates
+﻿using Newtonsoft.Json;
+using System;
+
+namespace GoogleMapsComponents.Maps.Coordinates
 {
     public class Padding
     {
+        [JsonProperty("top")]
+        public int Top { get; set; } = 0;
+
+        [JsonProperty("right")]
+        public int Right { get; set; } = 0;
+
+        [JsonProperty("left")]
+        public int Left { get; set; } = 0;
+
+
+        public Padding(int padding)
+        {
+            Top = padding;
+            Right = padding;
+            Left = padding;
+        }
+
+        public Padding(int top, int right, int left)
+        {
+            Top = top;
+            Right = right;
+            Left = left;
+        }
+
+        public Padding(int top, int left)
+        {
+            Top = top;
+            Left = left;
+        }
     }
 }
