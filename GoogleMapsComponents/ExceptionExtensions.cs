8c9e4a5329186cd6748b8cf7e67be4fe8a9411a7
Avi Turner <avi.turner@mobileye.com>
2023-07-19 09:20:55 +0300

    Handling JS Disconnected Exceptions when disposing map after circuit was disconnected

---
+++ GoogleMapsComponents/ExceptionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoogleMapsComponents
+{
+    public static class ExceptionExtensions
+    {
+
+
+        public static bool HasInnerExceptionsOfType<T>(this Exception ex) where T : Exception => ex.GetInnerExceptionsOfType<T>().Any();
+
+        public static IEnumerable<T> GetInnerExceptionsOfType<T>(this Exception ex) where T : Exception
+        {
+            var candidates = new[] { ex, ex.InnerException };
+
+            var aggEx = ex as AggregateException;
+            if (aggEx is not null)
+            {
+                var innerEceptions = aggEx?.InnerExceptions?.ToArray() ?? Array.Empty<Exception>();
+                candidates = candidates.Concat(innerEceptions).Where(ex => ex is not null).ToArray();
+
+            }
+            IEnumerable<T> exceptions = candidates.Select(ex => ex as T).Where(ex => ex is not null).Cast<T>().Distinct();
+            return exceptions;
+        }
+    }
+}
