2c54e97374dad95ac5d77731ca8cd527688df937
ValentasDeltamina <valentas@deltamina.lt>
2024-03-20 21:37:47 +0200

    Add class DragEndEvent for dragend listener (Suggestion) #321

---
+++ GoogleMapsComponents/Maps/DragEndEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace GoogleMapsComponents.Maps;
+
+public class DomEvent
+{
+    public bool IsTrusted { get; set; }
+}
+
+/// <summary>
+/// Used as await marker.AddListener<DragEndEvent>("dragend", (e) => OnDragEndDistribution(e));
+/// </summary>
+public class DragEndEvent
+{
+    public LatLngLiteral LatLng { get; set; }
+    public Point Pixel { get; set; }
+    public DomEvent DomEvent { get; set; }
+}
