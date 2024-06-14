c5a6d550ae98a4b67730bf70ce3f905f2b878f10
Valentas <valentas.mat@gmail.com>
2024-06-14 07:40:28 +0300

    Minor formating changes in client side demo

--- ClientSideDemo/Startup.cs
+++ ClientSideDemo/Startup.cs
@@ -5,17 +5,12 @@ namespace ClientSideDemo
 {
     public class Startup
     {
-
         private static async Task Main(string[] args)
         {
-
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
             builder.RootComponents.Add<App>("app");
-
             await builder.Build().RunAsync();
-
         }
-
     }
 }
