e5decfcdf81cb58c0fe54a282fe60bf94ea89d89
Valentas <valentas.mat@gmail.com>
2024-08-24 10:36:46 +0300

    Changed map version to beta in client sample

--- ClientSideDemo/Startup.cs
+++ ClientSideDemo/Startup.cs
@@ -1,16 +1,22 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using GoogleMapsComponents;
+using GoogleMapsComponents.Maps;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Threading.Tasks;
 
-namespace ClientSideDemo
-{
-    public class Startup
-    {
-        private static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+namespace ClientSideDemo;
 
-            builder.RootComponents.Add<App>("app");
-            await builder.Build().RunAsync();
-        }
+public class Startup
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+        builder.Services.AddBlazorGoogleMaps(new MapApiLoadOptions("AIzaSyBdkgvniMdyFPAcTlcZivr8f30iU-kn1T0")
+        {
+            Version = "beta"
+        });
+
+        builder.RootComponents.Add<App>("app");
+        await builder.Build().RunAsync();
     }
 }
