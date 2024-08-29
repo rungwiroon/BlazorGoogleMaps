e3ec796545b3a10515b9a28f68c4c992096c7bfc
Valentas <valentas.mat@gmail.com>
2024-08-29 17:38:50 +0300

    Added more test to test page

--- ServerSideDemo/Pages/TestAllPage.razor.cs
+++ ServerSideDemo/Pages/TestAllPage.razor.cs
@@ -1,5 +1,7 @@
 ﻿using GoogleMapsComponents;
 using GoogleMapsComponents.Maps;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,11 @@ namespace ServerSideDemo.Pages;
 
 public partial class TestAllPage
 {
+    [Inject] private IJSRuntime JsRuntime { get; set; } = null!;
     private GoogleMap _map1 = default!;
     private MapOptions _mapOptions = default!;
     private readonly Stack<Marker> _markers = new Stack<Marker>();
-    private int _zIndex;
     private MarkerClustering? _markerClustering;
-    private LatLngBounds _bounds;
     private readonly List<string> _events = new();
     protected override void OnInitialized()
     {
@@ -32,13 +33,15 @@ public partial class TestAllPage
         };
     }
 
-    private async Task AfterMapRender()
+    private Task AfterMapRender()
     {
-        _bounds = await LatLngBounds.CreateAsync(_map1.JsRuntime);
+        _events.Add("AfterMapRender trigered");
+        return Task.CompletedTask;
     }
 
     private async Task StartTesting()
     {
+        await JsRuntime.InvokeVoidAsync("alert", "Check console for errors");
         _events.Add("Testing started");
         _events.Add("Testing markers");
         await TestMarkers();
@@ -48,6 +51,42 @@ public partial class TestAllPage
         await TestShapes();
         _events.Add("Testing advanced marker");
         await TestAdvancedMarker();
+        _events.Add("Testing events. Move and click on map!!!");
+        await JsRuntime.InvokeVoidAsync("alert", "Move, drag and click on map!!!");
+        await TestMapEvents();
+    }
+
+    private async Task TestMapEvents()
+    {
+        await _map1.InteropObject.AddListener("center_changed", OnCenter);
+        await _map1.InteropObject.AddListener<MouseEvent>("click", OnClick);
+        await _map1.InteropObject.AddListener("dblclick", OnDoubleClick);
+        await _map1.InteropObject.AddListener("drag", OnDrag);
+
+        return;
+
+        void OnCenter()
+        {
+            AddEvent("Center changed");
+        }
+
+        void OnDoubleClick()
+        {
+            AddEvent("OnDoubleClicked ");
+        }
+
+        void OnDrag()
+        {
+            AddEvent("OnDrag");
+        }
+
+        void OnClick(MouseEvent x) => _events.Add($"OnDrag {x.LatLng}");
+    }
+
+    private void AddEvent(string eventName)
+    {
+        _events.Add(eventName);
+        StateHasChanged();
     }
 
     private async Task TestAdvancedMarker()
@@ -55,7 +94,7 @@ public partial class TestAllPage
         var mapCenter = await _map1.InteropObject.GetCenter();
         const string svg = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"56\" height=\"56\" viewBox=\"0 0 56 56\" fill=\"none\">\r\n  <rect width=\"56\" height=\"56\" rx=\"28\" fill=\"#7837FF\"></rect>\r\n  <path d=\"M46.0675 22.1319L44.0601 22.7843\" stroke=\"white\" stroke-width=\"2.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path>\r\n  <path d=\"M11.9402 33.2201L9.93262 33.8723\" stroke=\"white\" stroke-width=\"2.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path>\r\n  <path d=\"M27.9999 47.0046V44.8933\" stroke=\"white\" stroke-width=\"2.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path>\r\n  <path d=\"M27.9999 9V11.1113\" stroke=\"white\" stroke-width=\"2.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path>\r\n  <path d=\"M39.1583 43.3597L37.9186 41.6532\" stroke=\"white\" stroke-width=\"2.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path>\r\n  <path d=\"M16.8419 12.6442L18.0816 14.3506\" stroke=\"white\" stroke-width=\"2.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path>\r\n  <path d=\"M9.93262 22.1319L11.9402 22.7843\" stroke=\"white\" stroke-width=\"2.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path>\r\n  <path d=\"M46.0676 33.8724L44.0601 33.2201\" stroke=\"white\" stroke-width=\"2.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path>\r\n  <path d=\"M39.1583 12.6442L37.9186 14.3506\" stroke=\"white\" stroke-width=\"2.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path>\r\n  <path d=\"M16.8419 43.3597L18.0816 41.6532\" stroke=\"white\" stroke-width=\"2.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></path>\r\n  <path d=\"M28 39L26.8725 37.9904C24.9292 36.226 23.325 34.7026 22.06 33.4202C20.795 32.1378 19.7867 30.9918 19.035 29.9823C18.2833 28.9727 17.7562 28.0587 17.4537 27.2401C17.1512 26.4216 17 25.5939 17 24.7572C17 23.1201 17.5546 21.7513 18.6638 20.6508C19.7729 19.5502 21.1433 19 22.775 19C23.82 19 24.7871 19.2456 25.6762 19.7367C26.5654 20.2278 27.34 20.9372 28 21.8649C28.77 20.8827 29.5858 20.1596 30.4475 19.6958C31.3092 19.2319 32.235 19 33.225 19C34.8567 19 36.2271 19.5502 37.3362 20.6508C38.4454 21.7513 39 23.1201 39 24.7572C39 25.5939 38.8488 26.4216 38.5463 27.2401C38.2438 28.0587 37.7167 28.9727 36.965 29.9823C36.2133 30.9918 35.205 32.1378 33.94 33.4202C32.675 34.7026 31.0708 36.226 29.1275 37.9904L28 39Z\" fill=\"#FF7878\"></path>\r\n</svg>";
 
-        var marker = await AdvancedMarkerElement.CreateAsync(_map1.JsRuntime, new AdvancedMarkerElementOptions()
+        _ = await AdvancedMarkerElement.CreateAsync(_map1.JsRuntime, new AdvancedMarkerElementOptions()
         {
             Position = mapCenter,
             Map = _map1.InteropObject,
@@ -95,7 +134,7 @@ public partial class TestAllPage
         });
 
         var bounds = new LatLngBoundsLiteral(await GeRandomLatLng(), await GeRandomLatLng());
-        var rectangle = await Rectangle.CreateAsync(_map1.JsRuntime, new RectangleOptions()
+        _ = await Rectangle.CreateAsync(_map1.JsRuntime, new RectangleOptions()
         {
             Draggable = true,
             Editable = true,
@@ -103,7 +142,7 @@ public partial class TestAllPage
             Bounds = bounds
         });
 
-        var circle = await Circle.CreateAsync(_map1.JsRuntime, new CircleOptions()
+        _ = await Circle.CreateAsync(_map1.JsRuntime, new CircleOptions()
         {
             Draggable = true,
             Editable = true,
@@ -115,12 +154,11 @@ public partial class TestAllPage
 
     private async Task TestMarkers()
     {
-        var mapCenter = await _map1.InteropObject.GetCenter();
         var marker = await Marker.CreateAsync(_map1.JsRuntime, new MarkerOptions()
         {
             Position = await GeRandomLatLng(),
             Map = _map1.InteropObject,
-            ZIndex = _zIndex,
+            ZIndex = 10,
             //Icon = new Symbol()
             //{
             //    Path = "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z"
@@ -171,6 +209,7 @@ public partial class TestAllPage
         {
             LatLngBoundsLiteral.CreateOrExtend(ref boundsLiteral, literal);
         }
+
         await _map1.InteropObject.FitBounds(boundsLiteral, OneOf.OneOf<int, GoogleMapsComponents.Maps.Coordinates.Padding>.FromT0(1));
 
     }
