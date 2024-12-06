5601b7ccc2211c2350a7a9b712f3bb97dd9626d5
emorell96 <emorell96@hotmail.com>
2024-12-06 20:10:25 +0100

    Created a new folder called extensions to hold all the extension methods used. Created new files to hold the different kind of extensions. not a single file with thousands of lines.

---
+++ GoogleMapsComponents/Extensions/JsInvokeExtensions.cs
@@ -0,0 +1,64 @@
+﻿using GoogleMapsComponents.Maps.Strings;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using OneOf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GoogleMapsComponents.Extensions;
+internal static class JsInvokeExtensions
+{
+    internal static async Task<TRes> AddListener<TRes, T>(
+        this IJSRuntime jsRuntime,
+        EventCallback<T> callback, 
+        params object?[] args)
+    {
+        var callBackJs = jsRuntime.MakeCallbackJsFriendly(callback);
+        var jsFriendlyArgs = jsRuntime.MakeArgJsFriendly(args).Append(callBackJs);
+        
+
+        if (typeof(IJsObjectRef).IsAssignableFrom(typeof(TRes)))
+        {
+            var guid = await jsRuntime.InvokeAsync<string?>(Methods.InvokeWithReturnedObjectRef, jsFriendlyArgs);
+
+            return guid == null ? default : (TRes)JsObjectRefInstances.GetInstance(guid);
+        }
+
+        if (typeof(IOneOf).IsAssignableFrom(typeof(TRes)))
+        {
+            var resultObject = await jsRuntime.InvokeAsync<string>(Methods.InvokeWithReturnedObjectRef, jsFriendlyArgs);
+            object? result = null;
+
+            if (resultObject is string someText)
+            {
+                try
+                {
+                    var jo = JsonDocument.Parse(someText);
+                    var typeToken = jo.RootElement.GetProperty("dotnetTypeName").GetString();
+                    if (typeToken != null)
+                    {
+                        result = JsonExtensions.DeSerializeObject<TRes>(typeToken);
+                    }
+                    else
+                    {
+                        result = someText;
+                    }
+                }
+                catch
+                {
+                    result = someText;
+                }
+            }
+
+            return (TRes?)result;
+        }
+        else
+        {
+            return await jsRuntime.InvokeAsync<TRes>(Methods.InvokeWithReturnedObjectRef, jsFriendlyArgs);
+        }
+    }
+}
---
+++ GoogleMapsComponents/Extensions/JsonExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+using GoogleMapsComponents.Serialization;
+
+namespace GoogleMapsComponents.Extensions;
+internal static class JsonExtensions
+{
+    private static readonly JsonSerializerOptions Options = new JsonSerializerOptions()
+    {
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+
+    static JsonExtensions()
+    {
+        Options.Converters.Add(new OneOfConverterFactory());
+        Options.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+    }
+
+    public static object? DeSerializeObject(JsonElement json, Type type)
+    {
+        var obj = json.Deserialize(type, Options);
+        return obj;
+    }
+
+    public static T? DeSerializeObject<T>(JsonElement json)
+    {
+        return json.Deserialize<T>(Options);
+    }
+
+    public static object? DeSerializeObject(string? json, Type type)
+    {
+        if (json == null)
+        {
+            return default;
+        }
+
+        var obj = JsonSerializer.Deserialize(json, type, Options);
+        return obj;
+    }
+
+    public static TObject? DeSerializeObject<TObject>(string? json)
+    {
+        if (json == null)
+        {
+            return default;
+        }
+
+        var value = JsonSerializer.Deserialize<TObject>(json, Options);
+        return value;
+    }
+
+    public static string SerializeObject(object obj)
+    {
+        var value = JsonSerializer.Serialize(obj, Options);
+        return value;
+    }
+}
