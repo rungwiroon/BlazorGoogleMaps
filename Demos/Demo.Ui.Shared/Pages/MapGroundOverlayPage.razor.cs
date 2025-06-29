77e418455646f5475fbba58cbf687045899fcf50
Valentas <valentas.mat@gmail.com>
2025-06-29 10:31:44 +0300

    Moved demo ui pages to single project

---
+++ Demos/Demo.Ui.Shared/Pages/MapGroundOverlayPage.razor.cs
@@ -0,0 +1,44 @@
+﻿using GoogleMapsComponents;
+using GoogleMapsComponents.Maps;
+
+namespace Demo.Ui.Shared.Pages;
+
+public partial class MapGroundOverlayPage
+{
+    private GoogleMap _map1;
+    private MapOptions _mapOptions;
+
+    private readonly List<String> _events = new List<String>();
+
+    private GroundOverlay _mapoverlay;
+
+    protected override void OnInitialized()
+    {
+        _mapOptions = new MapOptions()
+        {
+            Zoom = 13,
+            Center = new LatLngLiteral(-74.166269, 40.719051),
+            MapTypeId = MapTypeId.Roadmap
+        };
+    }
+
+    private async Task RemoveOverlay()
+    {
+        await _mapoverlay.SetMap(null);
+    }
+    private async Task AddOverlay()
+    {
+        _mapoverlay = await GroundOverlay.CreateAsync(_map1.JsRuntime, "https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg", new LatLngBoundsLiteral()
+        {
+            North = 40.773941,
+            South = 40.712216,
+            East = -74.12544,
+            West = -74.22655
+        }, new GroundOverlayOptions()
+        {
+            Opacity = 0.5
+        });
+
+        await _mapoverlay.SetMap(_map1.InteropObject);
+    }
+}
---
+++ Demos/Demo.Ui.Shared/Pages/MapRoutes.razor.cs
@@ -0,0 +1,129 @@
+﻿using GoogleMapsComponents;
+using GoogleMapsComponents.Maps;
+
+namespace Demo.Ui.Shared.Pages;
+
+public partial class MapRoutes : IAsyncDisposable
+{
+    private GoogleMap? _map1;
+    private MapOptions _mapOptions;
+    private DirectionsRenderer _dirRend;
+    private string _durationTotalString;
+    private string _distanceTotalString;
+    private DirectionsResult _directionsResult;
+
+    protected override void OnInitialized()
+    {
+        _mapOptions = new MapOptions()
+        {
+            Zoom = 13,
+            Center = new LatLngLiteral(13.505892, 100.8162),
+            MapTypeId = MapTypeId.Roadmap
+        };
+    }
+
+    private async Task RemoveRoute()
+    {
+        await _dirRend.SetMap(null);
+
+        _durationTotalString = null;
+        _distanceTotalString = null;
+    }
+
+    private async Task OnAfterInitAsync()
+    {
+        //Create instance of DirectionRenderer
+        _dirRend = await DirectionsRenderer.CreateAsync(_map1.JsRuntime, new DirectionsRendererOptions()
+        {
+            Map = _map1.InteropObject,
+            MarkerOptions = new MarkerOptions()
+            {
+                Icon = new Symbol()
+                {
+                    Path = SymbolPath.BACKWARD_CLOSED_ARROW,
+                    Scale = 4,
+                    StrokeWeight = 2,
+                    FillColor = "white",
+                    FillOpacity = 1
+                }
+            }
+        });
+    }
+
+    private async Task AddDirections()
+    {
+        _durationTotalString = null;
+        _distanceTotalString = null;
+        if (await _dirRend.GetMap() is null)
+        {
+            await _dirRend.SetMap(_map1!.InteropObject);
+        }
+
+        //Adding a waypoint
+        var waypoints = new List<DirectionsWaypoint>();
+        waypoints.Add(new DirectionsWaypoint() { Location = "Bethlehem, PA", Stopover = true });
+
+        //Direction Request
+        var dr = new DirectionsRequest();
+        dr.Origin = "Allentown, PA";
+        dr.Destination = "Bronx, NY";
+        dr.Waypoints = waypoints;
+        dr.TravelMode = TravelMode.Driving;
+        dr.DrivingOptions = new DrivingOptions()
+        {
+            DepartureTime = DateTime.Now.AddHours(1)
+        };
+
+        //Calculate Route
+        _directionsResult = await _dirRend.Route(dr, new DirectionsRequestOptions()
+        {
+            StripLegsStepsLatLngs = false,
+            StripOverviewPath = false,
+            StripOverviewPolyline = false,
+            StripLegsStepsPath = false,
+            StripLegsSteps = false
+        });
+
+        if (_directionsResult is null)
+        {
+            return;
+        }
+        var routes = _directionsResult.Routes.SelectMany(x => x.Legs).ToList();
+
+        foreach (var route in routes)
+        {
+            _durationTotalString += route.DurationInTraffic?.Text;
+            _distanceTotalString += route.Distance.Text;
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_dirRend is not null)
+        {
+            await _dirRend.SetMap(null);
+            _dirRend.Dispose();
+        }
+    }
+
+    private async Task RunDirectionsService()
+    {
+        var service = await DirectionsService.CreateAsync(_map1!.JsRuntime);
+
+        DirectionsRequest request = new()
+        {
+            Origin = "Orlando, FL",
+            Destination = "Miami, FL"
+        };
+
+        var result = await service.Route(request);
+        if (result == null)
+        {
+            Console.WriteLine("No result");
+        }
+        else
+        {
+            Console.WriteLine(result.Routes?.FirstOrDefault()?.Summary ?? "No routes?");
+        }
+    }
+}
